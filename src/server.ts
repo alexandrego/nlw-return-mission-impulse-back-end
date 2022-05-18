0f99389954337a23f8f7f481259381918abee7a7
Alexandre Goncalves <alexandre_c_g@hotmail.com>
2022-05-18 07:57:13 -0300

    chore: Continua... 👨🏾‍💻

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import express from 'express'
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (request, response) => { //Uma função assincrono aguarda o processamento finalizar para devolver uma resposta ao usuario.
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({ //"await" aguarda o processo de criação terminar para poder entregar a resposta da função
+    //posso devolver para o front a resposta da operação com a criação da const acima.
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return response.status(201).json({ data: feedback });
+})
 
 app.listen(3333, () => {
   console.log('🚀 Server Online...')
